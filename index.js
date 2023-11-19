ea55da5e68da40f84d06ec4074e4479a3ef86454
shohruzjon_vl <shohruzjon@vanalauri.com>
2023-11-19 21:37:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const multer = require("multer");
+const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const upload = multer({ dest: "uploads/" });
+
+// Replace with your Telegram bot token
+const token = "6786130954:AAF-dzGGNm2lD-KqrQ0NPDPXdTUQOrWowzo";
+const bot = new TelegramBot(token, { polling: true });
+
+app.use(cors());
+
+// Endpoint to upload images
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+
+  // Replace with your chat ID
+  const chatId = "6612518419";
+
+  // Send the photo to the Telegram bot
+  bot
+    .sendPhoto(chatId, fs.createReadStream(req.file.path))
+    .then(() => {
+      fs.unlinkSync(req.file.path); // Delete the file after sending
+      res.send({ message: "Image sent to Telegram" });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error sending image to Telegram");
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
